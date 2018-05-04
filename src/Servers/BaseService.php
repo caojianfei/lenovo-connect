4c081e0d3bea2131a3690c452470089a70027256
caojianfei <120631902@qq.com>
2018-05-04 10:39:37 +0800

    网关切换https

--- src/Servers/BaseService.php
+++ src/Servers/BaseService.php
@@ -16,7 +16,7 @@ abstract class BaseService
      *
      * @var string
      */
-    protected $gatewayUrl = 'http://gla.lenovomm.com//httpOpenServer/serviceProvide';
+    protected $gatewayUrl = 'https://gla.lenovomm.com//httpOpenServer/serviceProvide';
 
     /**
      * 系统参数
@@ -171,6 +171,9 @@ abstract class BaseService
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
 
         $result = curl_exec($ch);
         curl_close($ch);
